02cc03a0ad417257f5dc6432a6145ffe67314bc3
Daphne Grasselly <daphne.grasselly@roche.com>
2023-04-26 18:31:46 +0200

    fix sys dependencies

--- scripts/install_sysdeps.sh
+++ scripts/install_sysdeps.sh
@@ -6,7 +6,6 @@ set -e
 # Takes in the debian dependencies list as the first argument
 dependencies="$1"
 dependencies="$(echo -e "${dependencies}" | tr -d '[:space:]')" # trim spaces
-dependencies=${dependencies//,/ } # replace comma separator by spaces
 
 export DEBIAN_FRONTEND=noninteractive
 export ACCEPT_EULA=Y
@@ -17,7 +16,7 @@ apt-get update -y
 # Install packages
 # expected word splitting - list of packages require it
 # shellcheck disable=SC2086
-apt-get install -q -y "${dependencies}"
+apt-get install -q -y $(echo "$dependencies" | tr ',' ' ')
 
 # Clean up
 apt-get autoremove -y
