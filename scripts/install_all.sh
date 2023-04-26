7dc55ab22fee7411712f60e31aace25daae574c9
Daphne Grasselly <daphne.grasselly@roche.com>
2023-04-26 17:48:54 +0200

    fix conditions

--- scripts/install_all.sh
+++ scripts/install_all.sh
@@ -5,19 +5,19 @@ renv_lock=$2 # renv lock file path or URL
 other_pkg=$3 # R pkg list
 repos=$4 # repos list
 
-if [ -z "${sysdeps}" ]
+if [ ! -z "${sysdeps}" ]
 then    
-        echo "Run install_sysdeps"
+        echo "Run install_sysdeps ${sysdeps}"
         /scripts/install_sysdeps.sh "${sysdeps}"
 fi
 
-if [ -z "${renv_lock}" ]
+if [ ! -z "${renv_lock}" ]
 then
         echo "Run restore renv"
         /scripts/restore_renv.sh
 fi
 
-if [ -z "${other_pkg}" ]
+if [ ! -z "${other_pkg}" ]
 then
         echo "Run install_other_pkgs - packages: ${other_pkg}"
         Rscript /scripts/install_other_pkgs.R "${other_pkg}" "${repos}"
