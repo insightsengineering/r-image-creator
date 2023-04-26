66a76db96ee8fde3f3afefedffd3cb9b687418fc
Daphne Grasselly <daphne.grasselly@roche.com>
2023-04-26 17:12:43 +0200

    add traces

--- scripts/install_all.sh
+++ scripts/install_all.sh
@@ -6,16 +6,19 @@ other_pkg=$3 # R pkg list
 repos=$4 # repos list
 
 if [ -z "${sysdeps}" ]
-then
+then    
+        echo "Run install_sysdeps"
         /scripts/install_sysdeps.sh "${sysdeps}"
 fi
 
 if [ -z "${renv_lock}" ]
 then
+        echo "Run restore renv"
         /scripts/restore_renv.sh
 fi
 
 if [ -z "${other_pkg}" ]
 then
+        echo "Run install_other_pkgs - packages: ${other_pkg}"
         Rscript /scripts/install_other_pkgs.R "${other_pkg}" "${repos}"
 fi
