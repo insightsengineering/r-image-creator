eaca2819aba24d4078f98e126ad32d7e1fe59485
Daphné Grasselly <56442075+dgrassellyb@users.noreply.github.com>
2023-11-27 16:48:37 +0100

    fix cp cmd

--- scripts/install_all.sh
+++ scripts/install_all.sh
@@ -28,6 +28,6 @@ fi
 
 if [ "${description}" != "None" ]; then
 	echo "Run install_pkgs_from_description"
-	cp $description ./DESCRIPTION
+	cp $description ./
 	Rscript -e "install.packages('devtools');devtools::install(force = TRUE)"
 fi
